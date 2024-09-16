1838f3a600c1fa21bc52299d7a9d2f82de3e02db
Ryan Hunter <ryanhunter237@gmail.com>
2024-09-16 14:08:27 -0400

    better inset.py

--- insert.py
+++ insert.py
@@ -4,10 +4,11 @@ import time
 
 import requests
 
-NUM_GROUP_IDS = 3
-FILES_PER_GROUP_ID = 4
+GROUP_IDS = ["".join(random.choices(string.hexdigits, k=32)) for _ in range(3)]
 EXTENSIONS = [".pdf", ".txt", ".png"]
+MD5s = ["".join(random.choices(string.hexdigits, k=32)) for _ in range(6)]
 TAGS = ["SMALL", "MEDIUM", "LARGE", "OLD", "NEW"]
+NUM_FILES = 16
 
 
 def insert_file(group_id, filename, md5):
@@ -21,19 +22,19 @@ def insert_tag(md5, sequence, tag):
 
 
 def insert_data():
-    for _ in range(NUM_GROUP_IDS):
-        group_id = "".join(random.choices(string.hexdigits, k=32))
-        for _ in range(FILES_PER_GROUP_ID):
-            base = "".join(random.choices(string.ascii_letters, k=12))
-            extension = random.choice(EXTENSIONS)
-            filename = base + extension
-            md5 = "".join(random.choices(string.hexdigits, k=32))
-            insert_file(group_id, filename, md5)
-            for i, tag in enumerate(random.choices(TAGS, k=2), start=1):
-                insert_tag(md5, i, tag)
-                time.sleep(0.2)
-            time.sleep(1)
-        time.sleep(2)
+    sequence = 1
+    for _ in range(NUM_FILES):
+        group_id = random.choice(GROUP_IDS)
+        base = "".join(random.choices(string.ascii_letters, k=12))
+        extension = random.choice(EXTENSIONS)
+        filename = base + extension
+        md5 = random.choice(MD5s)
+        insert_file(group_id, filename, md5)
+        for tag in random.choices(TAGS, k=2):
+            time.sleep(0.2)
+            insert_tag(md5, sequence, tag)
+            sequence += 1
+        time.sleep(1)
 
 
 if __name__ == "__main__":
